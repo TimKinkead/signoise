0f471d124aefce6ff25efdce20be38009853e280
Derek Soike <dsoike@gmail.com>
2016-03-22 16:47:04 -0800

    Social Seed Create Improvements
    Made it easier to create multiple facebook social seeds. Now you can go
    back to search results after clicking or creating.

--- client/modules/main/social-seeds/create/controller.js
+++ client/modules/main/social-seeds/create/controller.js
@@ -21,13 +21,16 @@ angular.module('app').controller('SocialSeedsCreateController', [
         };
 
         // save new social seed
-        $scope.save = function() {
+        var save = $scope.save = function(clbk) {
+            status.processing = true;
             $http.post('data/socialseed', seed)
                 .success(function(data) {
+                    status.processing = false;
+                    if (clbk) { return clbk(); }
                     $modalInstance.close(data);
                 })
                 .error(function(err) {
-                    // handle error
+                    status.errorMessage = 'Error! Please try again.\n'+err;
                 });
         };
 
@@ -53,9 +56,14 @@ angular.module('app').controller('SocialSeedsCreateController', [
             status.processing = true;
             status.errorMessage = null;
             $scope.facebookResults = $resource('data/socialseed/facebook/search').get(
-                {query: seed.facebook.title},
-                function() { status.processing = false; },
-                function(err) { status.processing = false; status.errorMessage = 'Error! Please try again.\n'+err; }
+                {query: seed.facebook.query},
+                function() { 
+                    status.processing = false;
+                },
+                function(err) { 
+                    status.processing = false;
+                    status.errorMessage = 'Error! Please try again.\n'+err;
+                }
             );
         };
 
@@ -63,12 +71,23 @@ angular.module('app').controller('SocialSeedsCreateController', [
         $scope.selectFacebook = function(fbItem) {
             $scope.facebookFields = ['id', 'name', 'category'];
             status.step = 2;
-            seed.facebook = {
-                id: fbItem.id,
-                name: fbItem.name,
-                category: (fbItem.category) ? fbItem.category : 'Group'
-            };
+            seed.facebook.id = fbItem.id;
+            seed.facebook.name = fbItem.name;
+            seed.facebook.category = (fbItem.category) ? fbItem.category : 'Group';
         };
+        
+        // go back to facebook search results
+        var step1Facebook = $scope.step1Facebook = function() {
+            status.step = 1;
+            delete seed.facebook.id;
+            delete seed.facebook.name;
+            delete seed.facebook.category;
+        };
+        
+        // save seed and go back to facebook search results
+        $scope.saveAndStep1Facebook = function() {
+            save(function() { step1Facebook(); });
+        };  
 
     }
 ]);
